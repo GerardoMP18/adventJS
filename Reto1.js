21a5fff9874494ae81c039ab6d1c46380b2deff2
Gerardo Marin Parra <43628789+GerardoMP18@users.noreply.github.com>
2022-12-05 10:57:33 -0500

    [fix] Version Mejorada

--- Reto1.js
+++ Reto1.js
@@ -39,3 +39,8 @@ function wrapping(gifts) {
     }
     return newArray;
 }
+
+// Mejorado 
+function wrapping(gifts) {
+  return gifts.map(gift => `${'*'.repeat(gift.length + 2)}\n*${gift}*\n${'*'.repeat(gift.length + 2)}`);
+}
